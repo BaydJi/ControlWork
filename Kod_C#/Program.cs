c022f21d6e278e2f84466e6305a4db2b75138746
BaydJi <svyatosha95@mail.ru>
2022-08-07 13:48:54 +0300

    Написали код к задаче

---
+++ Kod_C#/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] array = new string[5] {"123", "hay", "world", "125", "Program"};
+            Console.WriteLine("Заданный массив:");
+            PrintArray(array);
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный массив:");
+            IteratArray(array);
+        }
+
+        public static void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        public static void IteratArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3) Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
